d7e8e6c61bcce1981d31bd0f53b2b79126325df7
Sam <super8989@gmail.com>
2019-11-03 20:50:19 -0500

    added async await for the dad joke api

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class JokeList extends Component {
+	async componentDidMount() {
+		//Load jokes
+		let res = await axios.get("https://icanhazdadjoke.com/");
+		console.log(res);
+	}
+
 	render() {
 		return <h1>Dad Jokes</h1>;
 	}
