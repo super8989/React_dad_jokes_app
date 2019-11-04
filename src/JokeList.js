185bfcd521747b68f80df4c5f57e66be8b63462b
Sam <super8989@gmail.com>
2019-11-03 20:59:16 -0500

    set default number of jokes to get and set up an empty state where the jokes will be put into

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class JokeList extends Component {
+	static defaultProps = {
+		numJokesToGet: 10
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = { jokes: [] };
+	}
+
 	async componentDidMount() {
 		//Load jokes
+		let jokes = [];
+
 		let res = await axios.get("https://icanhazdadjoke.com/", {
 			headers: { Accept: "application/json" }
 		});
-		console.log(res);
+		console.log(res.data.joke);
 	}
 
 	render() {
