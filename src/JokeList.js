ea130fc6322ce24937c0f37360ca8a37bc04a3e9
Sam <super8989@gmail.com>
2019-11-04 19:58:07 -0500

    changed state to be an object to add votes

--- src/JokeList.js
+++ src/JokeList.js
@@ -20,7 +20,7 @@ class JokeList extends Component {
 			let res = await axios.get("https://icanhazdadjoke.com/", {
 				headers: { Accept: "application/json" }
 			});
-			jokes.push(res.data.joke);
+			jokes.push({ joke: res.data.joke, votes: 0 });
 		}
 		this.setState({ jokes: jokes });
 	}
@@ -38,7 +38,9 @@ class JokeList extends Component {
 
 				<div className='JokeList-jokes'>
 					{this.state.jokes.map(j => (
-						<div>{j}</div>
+						<div>
+							{j.joke} - {j.votes}
+						</div>
 					))}
 				</div>
 			</div>
