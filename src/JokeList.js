398a8e26e7d3b967132cd9ef8d2806b8feb76991
Sam <super8989@gmail.com>
2019-11-05 18:59:32 -0500

    styled loading spinner

--- src/JokeList.js
+++ src/JokeList.js
@@ -62,7 +62,7 @@ class JokeList extends Component {
 	render() {
 		if (this.state.loading) {
 			return (
-				<div className='spinner'>
+				<div className='JokeList-spinner'>
 					<i className='far fa-8x fa-laugh fa-spin' />
 					<h1 className='JokeList-title'>Loading...</h1>
 				</div>
